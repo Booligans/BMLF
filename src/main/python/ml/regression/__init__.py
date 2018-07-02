061868dc89090d23c6bd92d7d9bde467edb9817d
francloz <francloz@ucm.es>
2018-07-02 18:54:13 +0200

    ML structure and chosen tools.
    We will prioritize certain tools and add more as time goes on.

--- src/main/python/ml/regression/__init__.py
+++ src/main/python/ml/regression/__init__.py
@@ -1,1 +1,15 @@
 
+"""
+This package will contain the regression tools.
+The different regression techniques which will be covered are:
+    Linear Regression
+    Logistic Regression
+    Polynomial Regression
+    Stepwise Regression
+    Ridge Regression
+    Lasso Regression
+    ElasticNet Regression
+
+why?
+https://www.analyticsvidhya.com/blog/2015/08/comprehensive-guide-regression/
+"""
