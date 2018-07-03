a78d99dd5d28a310e0c2bfcf6e91bb4035f84970
francloz <francloz@ucm.es>
2018-07-03 12:51:55 +0200

    Simplified ElasticNet

--- src/main/python/ml/regression/__init__.py
+++ src/main/python/ml/regression/__init__.py
@@ -6,10 +6,16 @@ The different regression techniques which will be covered are:
     Logistic Regression
     Polynomial Regression
     Stepwise Regression
-    Ridge Regression
-    Lasso Regression
-    ElasticNet Regression
+
+    x Ridge Regression
+    x Lasso Regression
+    ->ElasticNet Regression
+
+    Jacknife Regression
 
 why?
-https://www.analyticsvidhya.com/blog/2015/08/comprehensive-guide-regression/
+All kinds: https://www.analyticsvidhya.com/blog/2015/08/comprehensive-guide-regression/
+Elasticnet over Lasso and Ridge: https://stats.stackexchange.com/questions/184029/what-is-elastic-net-regularization-and-how-does-it-solve-the-drawbacks-of-ridge
+Which to use: https://www.datasciencecentral.com/profiles/blogs/10-types-of-regressions-which-one-to-use
+Examples and plots : https://blog.datarobot.com/regularized-linear-regression-with-scikit-learn
 """
