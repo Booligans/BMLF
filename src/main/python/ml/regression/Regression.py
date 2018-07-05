e4baf5fe6e46760545b269f8f74766cba190bde2
Eduardo Rivero Rodríguez <eduriver@ucm.es>
2018-07-05 20:54:48 +0200

    Increased readability in Regression.py, modified docstrings to follow conventions.

--- src/main/python/ml/regression/Regression.py
+++ src/main/python/ml/regression/Regression.py
@@ -1,4 +1,3 @@
-
 from sklearn import linear_model
 from sklearn.model_selection import cross_val_predict, cross_val_score, cross_validate
 from copy import copy, deepcopy
@@ -18,8 +17,7 @@ Implementing Polynomial regression as Linear regression: https://stats.stackexch
 """
 
 class LinearModel:
-    """This class encapsulates a linear model. 
-    """
+    """This class encapsulates a linear model."""
     self.model_ = None
 
     def __init__(self, type = 'auto', ini_params = {}, X = None, y = None, avoid_overfitting = True):
@@ -49,29 +47,149 @@ class LinearModel:
         self.model_ = None
         
         if type == 'linear' or type == 'polynomial':
-            model_ = linear_model.LinearRegression(ini_params['fit_intercept'],ini_params['normalize'],ini_params['copy_x'],ini_params['n_jobs']) 
+            model_ = linear_model.LinearRegression(ini_params['fit_intercept'],
+                                                    ini_params['normalize'],
+                                                    ini_params['copy_x'],
+                                                    ini_params['n_jobs']) 
         elif type == 'logistic':
-            model_ = linear_model.LogisticRegression(ini_params['penalty'],ini_params['dual'],ini_params['tolerance'],ini_params['c'],ini_params['fit_intercept'],ini_params['class_weight'],ini_params['random_state'],ini_params['solver'],ini_params['max_iterations'],ini_params['multi_class'],ini_params['verbose'],ini_params['warm_start'],ini_params['n_jobs'])
+            model_ = linear_model.LogisticRegression(ini_params['penalty'],
+                                                     ini_params['dual'],
+                                                     ini_params['tolerance'],
+                                                     ini_params['c'],
+                                                     ini_params['fit_intercept'],
+                                                     ini_params['class_weight'],
+                                                     ini_params['random_state'],
+                                                     ini_params['solver'],
+                                                     ini_params['max_iterations'],
+                                                     ini_params['multi_class'],
+                                                     ini_params['verbose'],
+                                                     ini_params['warm_start'],
+                                                     ini_params['n_jobs'])
         elif type == 'logisticcv':
-            model_ = linear_model.LogisticRegressionCV(ini_params['cs'],ini_params['fit_intercept'],ini_params['cv'],ini_params['dual'],ini_params['penalty'],ini_params['scoring'],ini_params['solver'],ini_params['tol'],ini_params['max_iter'],ini_params['class_weight'],ini_params['n_jobs'],ini_params['verbose'],ini_params['refit'],ini_params['intercept_scaling'],ini_params['multi_class'],ini_params['random_state'])
+            model_ = linear_model.LogisticRegressionCV(ini_params['cs'],
+                                                       ini_params['fit_intercept'],
+                                                       ini_params['cv'],ini_params['dual'],
+                                                       ini_params['penalty'],ini_params['scoring'],
+                                                       ini_params['solver'],ini_params['tol'],
+                                                       ini_params['max_iter'],
+                                                       ini_params['class_weight'],
+                                                       ini_params['n_jobs'],
+                                                       ini_params['verbose'],
+                                                       ini_params['refit'],
+                                                       ini_params['intercept_scaling'],
+                                                       ini_params['multi_class'],
+                                                       ini_params['random_state'])
         elif type == 'elasticnet':
-            model_ = linear_model.ElasticNet(ini_params['alpha'],ini_params['l1_ratio'],ini_params['fit_intercept'],ini_params['normalize'],ini_params['precompute'],ini_params['max_iter'],ini_params['copy_x'],ini_params['tol'],ini_params['warm_start'],ini_params['positive'],ini_params['random_state'],ini_params['selection']) 
+            model_ = linear_model.ElasticNet(ini_params['alpha'],
+                                             ini_params['l1_ratio'],
+                                             ini_params['fit_intercept'],
+                                             ini_params['normalize'],
+                                             ini_params['precompute'],
+                                             ini_params['max_iter'],
+                                             ini_params['copy_x'],
+                                             ini_params['tol'],
+                                             ini_params['warm_start'],
+                                             ini_params['positive'],
+                                             ini_params['random_state'],
+                                             ini_params['selection']) 
         elif type == 'elasticnetcv':
-            model_ = linear_model.ElasticNetCV(ini_params['l1_ratio'],ini_params['eps'],ini_params['n_alphas'],ini_params['alphas'],ini_params['fit_intercept'],ini_params['normalize'],ini_params['precompute'],ini_params['max_iter'],ini_params['tol'],ini_params['cv'],ini_params['copy_c'],ini_params['verbose'],ini_params['n_jobs'],ini_params['positive'],ini_params['random_state'],ini_params['selection'])
+            model_ = linear_model.ElasticNetCV(ini_params['l1_ratio'],
+                                               ini_params['eps'],
+                                               ini_params['n_alphas'],
+                                               ini_params['alphas'],
+                                               ini_params['fit_intercept'],
+                                               ini_params['normalize'],
+                                               ini_params['precompute'],
+                                               ini_params['max_iter'],
+                                               ini_params['tol'],
+                                               ini_params['cv'],
+                                               ini_params['copy_c'],
+                                               ini_params['verbose'],
+                                               ini_params['n_jobs'],
+                                               ini_params['positive'],
+                                               ini_params['random_state'],
+                                               ini_params['selection'])
         elif type == 'bayesian':
-            model_ = linear_model.BayesianRidge(ini_params['n_iter'],ini_params['tol'],ini_params['alpha_1'],ini_params['alpha_2'],ini_params['lambda_1'],ini_params['lambda_2'],ini_params['compute_score'],ini_params['fit_intercept'],ini_params['normalize'],ini_params['copy_x'],ini_params['verbose'])
+            model_ = linear_model.BayesianRidge(ini_params['n_iter'],
+                                                ini_params['tol'],
+                                                ini_params['alpha_1'],
+                                                ini_params['alpha_2'],
+                                                ini_params['lambda_1'],
+                                                ini_params['lambda_2'],
+                                                ini_params['compute_score'],
+                                                ini_params['fit_intercept'],
+                                                ini_params['normalize'],
+                                                ini_params['copy_x'],
+                                                ini_params['verbose'])
         elif type == 'orthogonal':
-            model_ = linear_model.OrthogonalMatchingPursuit(ini_params['n_nonzero_coefs'],ini_params['tol'],ini_params['fit_intercept'],ini_params['normalize'], ini_params['precompute']) 
+            model_ = linear_model.OrthogonalMatchingPursuit(ini_params['n_nonzero_coefs'],
+                                                            ini_params['tol'],
+                                                            ini_params['fit_intercept'],
+                                                            ini_params['normalize'],
+                                                            ini_params['precompute']) 
         elif type == 'orthogonalcv':
-            model_ = linear_model.OrthogonalMatchingPursuitCV(ini_params['copy'],ini_params['fit_intercept'],ini_params['true'],ini_params['max_iter'],ini_params['cv'],ini_params['n_jobs'],ini_params['verbose'])
+            model_ = linear_model.OrthogonalMatchingPursuitCV(ini_params['copy'],
+                                                              ini_params['fit_intercept'],
+                                                              ini_params['true'],
+                                                              ini_params['max_iter'],
+                                                              ini_params['cv'],
+                                                              ini_params['n_jobs'],
+                                                              ini_params['verbose'])
         elif type == 'theil':
-            model_ = linear_model.TheilSenRegressor(ini_params['fit_intercept'],ini_params['copy_x'],ini_params['max_population'],ini_params['n_subsamples'],ini_params['max_iter'],ini_params['tol'],ini_params['random_state'],ini_params['n_jobs'],ini_params['verbose'])
+            model_ = linear_model.TheilSenRegressor(ini_params['fit_intercept'],
+                                                    ini_params['copy_x'],
+                                                    ini_params['max_population'],
+                                                    ini_params['n_subsamples'],
+                                                    ini_params['max_iter'],
+                                                    ini_params['tol'],
+                                                    ini_params['random_state'],
+                                                    ini_params['n_jobs'],
+                                                    ini_params['verbose'])
         elif type == 'sgd':
-            model_ = linear_model.SGDRegressor(ini_params['loss'],ini_params['penalty'],ini_params['alpha'],ini_params['l1_ratio'],ini_params['fit_intercept'],ini_params['max_iter'],ini_params['tol'],ini_params['shuffle'],ini_params['verbose'],ini_params['epsilon'],ini_params['random_state'],ini_params['learning_rate'],ini_params['eta0'],ini_params['power_t'],ini_params['warm_start'],ini_params['average'],ini_params['n_iter'])
+            model_ = linear_model.SGDRegressor(ini_params['loss'],
+                                               ini_params['penalty'],
+                                               ini_params['alpha'],
+                                               ini_params['l1_ratio'],
+                                               ini_params['fit_intercept'],
+                                               ini_params['max_iter'],
+                                               ini_params['tol'],
+                                               ini_params['shuffle'],
+                                               ini_params['verbose'],
+                                               ini_params['epsilon'],
+                                               ini_params['random_state'],
+                                               ini_params['learning_rate'],
+                                               ini_params['eta0'],
+                                               ini_params['power_t'],
+                                               ini_params['warm_start'],
+                                               ini_params['average'],
+                                               ini_params['n_iter'])
         elif type == 'perceptron':
-            model_ = linear_model.Perceptron(ini_params['penalty'],ini_params['alpha'],ini_params['fit_intercept'],ini_params['max_iter'],ini_params['tol'],ini_params['shuffle'],ini_params['verbose'],ini_params['eta0'],ini_params['n_jobs'],ini_params['random_state'],ini_params['class_weight'],ini_params['warm_start'],ini_params['n_iter'])
+            model_ = linear_model.Perceptron(ini_params['penalty'],
+                                             ini_params['alpha'],
+                                             ini_params['fit_intercept'],
+                                             ini_params['max_iter'],
+                                             ini_params['tol'],
+                                             ini_params['shuffle'],
+                                             ini_params['verbose'],
+                                             ini_params['eta0'],
+                                             ini_params['n_jobs'],
+                                             ini_params['random_state'],
+                                             ini_params['class_weight'],
+                                             ini_params['warm_start'],
+                                             ini_params['n_iter'])
         elif type == 'passive_aggressive':
-            model_ = linear_model.PassiveAggressiveRegressor(ini_params['c'],ini_params['fit_intercept'],ini_params['max_iter'],ini_params['tol'],ini_params['shuffle'],ini_params['verbose'],ini_params['loss'],ini_params['epsilon'],ini_params['random_state'],ini_params['warm_start'],ini_params['average'],ini_params['n_iter'])
+            model_ = linear_model.PassiveAggressiveRegressor(ini_params['c'],
+                                                             ini_params['fit_intercept'],
+                                                             ini_params['max_iter'],
+                                                             ini_params['tol'],
+                                                             ini_params['shuffle'],
+                                                             ini_params['verbose'],
+                                                             ini_params['loss'],
+                                                             ini_params['epsilon'],
+                                                             ini_params['random_state'],
+                                                             ini_params['warm_start'],
+                                                             ini_params['average'],
+                                                             ini_params['n_iter'])
         elif type == 'auto':
             choose_model(X,y)
         else:
@@ -109,7 +227,7 @@ class LinearModel:
 
     def score(self, X):
         """
-        Returns the coefficient of determination R^2 of the prediction.
+        Returns the R^2 coefficient of the prediction.
 
         :param X: data
         :type X: ndarray or scipy.sparse matrix, (n_samples, n_features)
@@ -117,20 +235,15 @@ class LinearModel:
         self.model_.score(X)
 
     def set_parameters(self,parameters):
-        """
-        Sets the parameters of a model.
-        """
+        """Sets the parameters of a model."""
         return self.model_.set_paraeters(parameters)
 
     def get_params(self):
-        """
-        Gets the parameters of the model.
-        """
+        """Gets the parameters of the model."""
         self.model_.get_params()
 
     def compare(self, model : LinearModel, X, y = None):
-        """
-        Compares the score of a sample in two models.
+        """Compares the score of a sample in two models.
         Returns a crossvalidation of metrics, predictions and score.
         
         :param model: model
@@ -144,8 +257,7 @@ class LinearModel:
         return metric, predictions, score
 
     def choose_model(self,X,y = None):
-        """
-        Automatic model chooser.
+        """Automatic model chooser.
 
         :param X: data
         :param y: target
@@ -154,7 +266,13 @@ class LinearModel:
         :type y: ndarray, shape (n_samples,) or (n_samples, n_targets)
         """
         #{'linear', 'polynomial',logistic','logisticcv','elasticnet','elasticnetcv','orthogonal','orthogonalcv','theil','sgd','perceptron','passive_aggressive'}
-        models = {'linear':linear_model.LinearRegression(),'logistic':linear_model.LogisticRegression(),'elasticnet':linear_model.ElasticNet(),'orthogonal':linear_model.OrthogonalMatchingPursuit(),'theil':linear_model.TheilSenRegressor(),'sgd':linear_model.SGDRegressor(),'passive_agressive':linear_model.PassiveAggressiveRegressor()}
+        models = {'linear':linear_model.LinearRegression(),
+                  'logistic':linear_model.LogisticRegression(),
+                  'elasticnet':linear_model.ElasticNet(),
+                  'orthogonal':linear_model.OrthogonalMatchingPursuit(),
+                  'theil':linear_model.TheilSenRegressor(),
+                  'sgd':linear_model.SGDRegressor(),
+                  'passive_agressive':linear_model.PassiveAggressiveRegressor()}
         scores = {}
         for name,model in models:
             scores[name] = []
