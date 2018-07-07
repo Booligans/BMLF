dd9bc86f97d0a3345a8082bde09e1f5ac0e17281
tito_kimbo <eduriver@ucm.es>
2018-07-07 16:47:57 +0200

    Added documentation for main plotting module classes.

--- src/main/python/plots/basic_plots.py
+++ src/main/python/plots/basic_plots.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 import plot
 
 class BarPlot(Plot):
+    """Class encapsulating a BarPlot."""
     
     def __init__(self, name, data, type, x_label=None, y_label=None):
         super().__init__(name,data,type,x_label,y_label)
@@ -11,7 +12,7 @@ class BarPlot(Plot):
 
 
 class PieChart(Plot):
-    
+    """Class encapsulating a PieChart."""
     def __init__(self, name, data, type, x_label=None, y_label=None):
         super().__init__(name,data,type,x_label,y_label)
     
@@ -20,7 +21,7 @@ class PieChart(Plot):
 
 
 class LinePlot(Plot):
-    
+    """Class encapsulating a LinePlot."""
     def __init__(self, name, data, type, x_label=None, y_label=None):
         super().__init__(name,data,type,x_label,y_label)
     
@@ -29,7 +30,7 @@ class LinePlot(Plot):
         
 
 class Plot3D(Plot):
-    
+    """Class encapsulating a 3D Plot."""
     def __init__(self, name, data, type, x_label=None, y_label=None):
         super().__init__(name,data,type,x_label,y_label)
     
--- src/main/python/plots/plotting_service.py
+++ src/main/python/plots/plotting_service.py
@@ -1,8 +1,20 @@
 class PlottingService:
-    
+    """Class that acts as a Plot factory in the BMLF framework."""
     
     def __init__(self):
+        """Class constructor"""
         pass
         
     def build_plot(project, data, type, *args, **kwargs):
+        """Builds a plot of the indicated type from the given data.
+        
+        :param project: current project
+        :type project: Project
+        :param data: data to plot
+        :type data: ndarray
+        :param type: type of plot
+        :type type: str
+        :param *args: Variable length argument list
+        :param **kwargs: Arbitrary keyword arguments
+        """
         pass
