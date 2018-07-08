1af78e3e4ef5f02f652d20c3714e699c50ee4c9f
tito_kimbo <eduriver@ucm.es>
2018-07-08 23:33:39 +0200

    Implemented basic plot creation and histograms.

--- src/main/python/plots/statistical_plots.py
+++ src/main/python/plots/statistical_plots.py
@@ -2,14 +2,16 @@ from matplotlib import pyplot as plt
 import plot
 
 class Histogram(Plot):
+    """Class encapsulating a Histogram."""
     
     def __init__(self, name, data, type, x_label=None, y_label=None):
         super().__init__(name,data,type,x_label,y_label)
     
-    def plot():
-        pass
+    def plot(self):
+        hist(self.data)
 
 class BoxPlot(Plot):
+    """Class encapsulating a Box/Box and Whiskers plot."""
     
     def __init__(self, name, data, type, x_label=None, y_label=None):
         super().__init__(name,data,type,x_label,y_label)
