582a701425a3b7478bd32a37a999b4f0b62831a6
MBrianLC <mbrianlc@gmail.com>
2018-07-13 17:00:04 +0200

    New module Menu

--- src/main/python/ui/gui.py
+++ src/main/python/ui/gui.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from dialog import *
 import os
 
 class Ui_MainWindow(object):
@@ -24,9 +25,6 @@ class Ui_MainWindow(object):
         self.graphicsView.setGeometry(QtCore.QRect(0, 0, 801, 551))
         self.graphicsView.setObjectName("graphicsView")
         self.tabWidget.addTab(self.tab_2, "")
-        self.tab_3 = QtWidgets.QWidget()
-        self.tab_3.setObjectName("tab_3")
-        self.tabWidget.addTab(self.tab_3, "")
         file = str(QtWidgets.QFileDialog.getExistingDirectory(MainWindow, "Select your workspace"))
         self.projectTreeWidget = projectTree(self.centralwidget, file)
         MainWindow.setCentralWidget(self.centralwidget)
@@ -43,16 +41,12 @@ class Ui_MainWindow(object):
         self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def resizeEvent(self, MainWindow):
-        MainWindow.resize(500, 200)
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "BMLF"))
         self.tableWidget.setSortingEnabled(False)
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Data"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Visualization"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "Analysis"))
 			
 class ToolButton(QtWidgets.QToolButton):
     def __init__(self, *args):
@@ -109,8 +103,15 @@ class MenuBar(QtWidgets.QMenuBar):
         actionOpen = Action(self, "actionOpen", lambda: self.opened(), "Open")
         self.menuNew = QtWidgets.QMenu(self)
         self.menuNew.setObjectName("menuNew")
-        actionMLModule = Action(self, "actionMLModule", lambda: self.newML(args[1]), "ML Module")
+        actionMLModule = Action(self, "actionMLModule", lambda: self.newML(args[1], "ML"), "ML Module")
+        actionAModule = Action(self, "actionAModule", lambda: self.newML(args[1], "Analysis"), "Analysis")
+        actionMLModule = Action(self, "actionMLModule", lambda: self.newML(args[1], "ML"), "ML Module")
         actionSwWorkspace = Action(self, "actionSwWorkspace", lambda: self.sw_workspace(args[1]), "Switch Workspace")
+        self.menuPreferences = QtWidgets.QMenu(self)
+        self.menuPreferences.setObjectName("menuPreferences")
+        self.setPreferencesAction = Action(self, "setPreferencesAction", lambda: self.dialog(args[1]), "Preferences")
+        self.menuPreferences.addAction(self.setPreferencesAction)
+        self.addAction(self.menuPreferences.menuAction())
         actionUndo = Action(self, "actionUndo", lambda: self.undo(), "Undo")
         actionRedo = Action(self, "actionRedo", lambda: self.redo(), "Redo")
         self.menuNew.addAction(actionMLModule)
@@ -127,6 +128,7 @@ class MenuBar(QtWidgets.QMenuBar):
         self.menuFile.setTitle(_translate("MainWindow", "File"))
         self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
         self.menuNew.setTitle(_translate("MainWindow", "New"))
+        self.menuPreferences.setTitle(_translate("MainWindow", "Preferences"))
 
     def opened(self):
         QtWidgets.QFileDialog.getOpenFileName()
@@ -134,7 +136,7 @@ class MenuBar(QtWidgets.QMenuBar):
     def sw_workspace(self, Ui_MainWindow):
         file = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select your workspace"))
         Ui_MainWindow.projectTreeWidget.reset()
-        Ui_MainWindow.projectTreeWidget.load_project_structure(file, Ui_MainWindow.projectTreeWidget)
+        Ui_MainWindow.projectTreeWidget.load_project(file, Ui_MainWindow.projectTreeWidget)
 	
     def undo(self):
         print("undo")
@@ -142,13 +144,13 @@ class MenuBar(QtWidgets.QMenuBar):
     def redo(self):
         print("redo")
 
-    def newModule(self):
-	    Option = Ui_Dialog()
-
-    def newML(self, Ui_MainWindow):
+    def newModule(self, Ui_MainWindow, MName):
         self.tab_new = QtWidgets.QWidget()
-        self.tab_new.setObjectName("tab")
-        Ui_MainWindow.tabWidget.addTab(self.tab_new, "ML")
+        self.tab_new.setObjectName(MName)
+        Ui_MainWindow.tabWidget.addTab(self.tab_new, MName)
+	
+    def dialog(self, Ui_MainWindow):
+        dialog = PreferencesDialog(Ui_MainWindow)
 
 class projectTree(QtWidgets.QTreeWidget):
     def __init__(self, *args):
@@ -157,7 +159,7 @@ class projectTree(QtWidgets.QTreeWidget):
         self.setObjectName("projectTreeWidget")
         self.setHeaderLabel("Projects")
         self.setStyleSheet( "QTreeWidget{background: rgb(216,233,250);}")
-        self.load_project_structure(args[1], self)
+        self.load_project(args[1], self)
 
     def reset(self):
         iterator = QtWidgets.QTreeWidgetItemIterator(self, QtWidgets.QTreeWidgetItemIterator.All)
@@ -169,7 +171,7 @@ class projectTree(QtWidgets.QTreeWidget):
             self.takeTopLevelItem(i)
             i -= 1
 
-    def load_project_structure(self, startpath, tree):
+    def load_project(self, startpath, tree):
         """
         Load Project structure tree
         :param startpath: 
@@ -180,27 +182,10 @@ class projectTree(QtWidgets.QTreeWidget):
             path_info = startpath + "/" + element
             parent_itm = QtWidgets.QTreeWidgetItem(tree, [os.path.basename(element)])
             if os.path.isdir(path_info):
-                self.load_project_structure(path_info, parent_itm)
+                self.load_project(path_info, parent_itm)
                 parent_itm.setIcon(0, QtGui.QIcon('assets/folder.png'))
             else:
                 parent_itm.setIcon(0, QtGui.QIcon('assets/file.png'))
-
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(508, 300)
-        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
-        self.buttonBox.setGeometry(QtCore.QRect(150, 250, 341, 32))
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
-        self.sl_value = QtWidgets.QSlider(Dialog)
-        self.sl_value.setGeometry(QtCore.QRect(220, 120, 161, 31))
-        self.sl_value.setOrientation(QtCore.Qt.Horizontal)
-        self.sl_value.setObjectName("sl_value")
-        self.buttonBox.accepted.connect(Dialog.accept)
-        self.buttonBox.rejected.connect(Dialog.reject)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
 class Action(QtWidgets.QAction):
     def __init__(self, *args):
@@ -208,4 +193,24 @@ class Action(QtWidgets.QAction):
         self.setObjectName(args[1])
         self.triggered.connect(args[2])
         self.setText(QtCore.QCoreApplication.translate("MainWindow", args[3]))
+
+class PreferencesDialog(QtWidgets.QDialog):
+    def __init__(self, *args):
+        QtWidgets.QDialog.__init__(self)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.ui.checkBox.stateChanged.connect(lambda: self.state_changed(self.ui.checkBox, args[0], "ML Module"))
+        self.ui.checkBox_2.stateChanged.connect(lambda: self.state_changed(self.ui.checkBox_2, args[0], "Plotting"))
+        self.ui.checkBox_3.stateChanged.connect(lambda: self.state_changed(self.ui.checkBox_3, args[0], "Stat Analysis"))
+        self.ui.checkBox_4.stateChanged.connect(lambda: self.state_changed(self.ui.checkBox_4, args[0], "Data Analysis"))
+        self.exec_()
+
+    def state_changed(self, CheckBox, Ui_MainWindow, MName):
+        tab = Ui_MainWindow.tabWidget
+        if CheckBox.isChecked():
+            tab.removeTab(tab.indexOf(tab.findChild(QtWidgets.QWidget, MName)))
+            Ui_MainWindow.menubar.newModule(Ui_MainWindow, MName)
+        else:
+            tab.removeTab(tab.indexOf(tab.findChild(QtWidgets.QWidget, MName)))
 
