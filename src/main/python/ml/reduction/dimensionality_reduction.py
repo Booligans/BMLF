a583845425907d43fc68956cfd60919f3e47fdc6
tito_kimbo <eduriver@ucm.es>
2018-07-16 00:11:56 +0200

    Added PCAReductor class. Pending fixes for *args and **kwargs usage.

---
+++ src/main/python/ml/reduction/dimensionality_reduction.py
@@ -0,0 +1,23 @@
+from sklearn import decomposition
+
+class PCAReductor:
+    
+    def __init__(self, type='auto', *args, **kwargs):
+        
+        if type=='auto':
+            self.model = decomposition.PCA(*args,**kwargs)
+        elif type=='incremental':
+            self.model = decomposition.IncrementalPCA(*args,**kwargs)
+        elif type=='kernel':
+            self.model = decomposition.KernelPCA(*args,**kwargs)
+        else:
+            raise ValueError('The type ', type, ' does not exist.')
+            
+    def fit(self,data):
+        self.model.fit(data)
+        
+    def transform(self,data):
+        return self.model.transform(data)
+    
+    def fit_transform(self,data):
+        return self.model.fit_transform(data)
