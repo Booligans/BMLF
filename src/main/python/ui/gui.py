e82311baea2de3c697db0f989c0c61edc1323c86
Eduardo Rivero Rodríguez <eduriver@ucm.es>
2018-07-16 20:48:23 +0200

    Undid changes in UI imports.

--- src/main/python/ui/gui.py
+++ src/main/python/ui/gui.py
@@ -1,19 +1,11 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import mainWindow
+from ui.mainWindow import *
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
-        self.ui = mainWindow.Ui_MainWindow()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        
-if __name__ == '__main__':
-    print('Launching app')
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    if (window.ui.file != ""):
-        window.show()
-        app.exec_()
