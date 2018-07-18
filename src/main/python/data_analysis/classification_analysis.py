9c4fe5f30aac54c10d56c169825a21bdcf24da97
Eduardo Rivero Rodríguez <eduriver@ucm.es>
2018-07-18 14:24:07 +0200

    Advanced in ML Analysis and Classification Analysis code and design

--- src/main/python/data_analysis/classification_analysis.py
+++ src/main/python/data_analysis/classification_analysis.py
@@ -1,12 +1,21 @@
 import ml_analysis
+from sklearn.metrics import classification_report, confusion_matrix
 
 class ClassificationAnalysis(MLAnalysis):
     """Class encapsulating a Classification Analysis on certain data."""
     
-    def __init__(self, model, data):
-        super().__init__(model, data)
+    def __init__(self, model, test_data, test_labels):
+        super().__init__(model, test_data)
+        self.report = classification_report(test_labels, self.predictions) 
+        self.confusion_matrix = confusion_matrix(test_labels, self.predictions)
         
     def analyze():
         super().analyze()
-        self.title = 'Classification analysis for ML model ' + model + '.'
         #Decision boundary analysis
+        #------ ?
+        #accuracy, f1, precision, confusion matrix and other metrics report here
+        self.text.append('Classification report for test data')
+        self.text.append(str(self.report))
+        self.text.append('Confusion matrix for test data')
+        self.test.append(str(self.confusion_matrix))
+        #log loss, Mean Absolute Error, Mean Squared Error, R^2 pending
--- src/main/python/data_analysis/ml_analysis.py
+++ src/main/python/data_analysis/ml_analysis.py
@@ -1,21 +1,24 @@
 import data_analysis
 
 class MLAnalysis(DataAnalysis):
+"""This class encapsulates the analysis of the model for some test data."""
 
-    def __init__(self, model, data):
+    def __init__(self, model, test_data):
         """Class constructor.
         
         :param model: predictive model.
         :type model: MlModel
         """
         self.model = model
-        self.to_predict = data
-        self.predictions = self.model.predict(data)
-        #is score assignment uniform across models?
+        self.to_predict = test_data
+        self.predictions = self.model.predict(test_data)
         self.analyze()
     
     def analyze():
         self.text = []
-        #self.append('Number of items: ', len(data)) #rows of data matrix
-        #self.append('Number of features: ', len(data[0])) #columns of data matrix 
-        #accuracy, f1, precision, confusion matrix and other metrics report here
+        self.text.append('Number of items: ', len(data)) #rows of data matrix
+        self.text.append('Number of features: ', len(data[0])) #columns of data matrix 
+        #Model and parameters
+        self.text.append('Model: ', self.model)
+        #----- params?
+        
