b4ccd584e716c66f5b401b3cfea428ad3a043bb7
Eduardo Rivero Rodríguez <eduriver@ucm.es>
2018-07-20 11:23:32 +0200

    Added plotting support for *args and **kwargs for positional arguments.
    Note that given argument are permanent for the specific plot object created.

--- src/main/python/plots/plot.py
+++ src/main/python/plots/plot.py
@@ -3,7 +3,7 @@ from abc import ABC,abstractmethod
 class Plot(ABC):
     """This class represents an abstract plot."""
     
-    def __init__(self, name, data, type, x_label=None, y_label=None):
+    def __init__(self, name, data, type, x_label=None, y_label=None, *args, **kwargs):
         """Class constructor.
         
         :param name
@@ -22,6 +22,8 @@ class Plot(ABC):
         self.type = type
         self.x_label = x_label
         self.y_label = y_label
+        self.args = args
+        slef.kwargs = kwargs
         
     @abstractmethod
     def plot(self, axes):
@@ -32,7 +34,7 @@ class Plot(ABC):
 class MultiPlot:
     """This class represents a plot with multiple subplots."""
     
-    def __init__(self, name, plots=[]):
+    def __init__(self, name, plots=[], *args, **kwargs):
         """Class constructor.
         
         :param name: name of the MultiPlot
@@ -42,6 +44,8 @@ class MultiPlot:
         """
         self.name = name
         self.plots = plots
+        self.args = args
+        self.kwargs = kwargs
         
     def add_plot(self,plot):
         """Adds a plot to the current MultiPlot.
