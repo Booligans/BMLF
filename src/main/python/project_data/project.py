d1bf57d2857bf26e4bb2f39bf7440d018715f5e6
Eduardo Rivero Rodríguez <eduriver@ucm.es>
2018-09-11 11:04:21 +0200

    Advanced in workspace and project management.

--- src/main/python/project_data/project.py
+++ src/main/python/project_data/project.py
@@ -1,10 +1,19 @@
-from ..plots.plot import Multiplot
 import os
 import datetime
+from ..plots.plot import Multiplot
 
 class Project:
+    """Class encapsulating project data."""
+    
+    def __init__(self): 
+        """Empty constructor."""
+        pass
     
     def __init__(self, name):
+        """Project constructor with name.
+        
+        :param name: Name of the project.
+        """
         self.name = name
         self.analysis = []
         self.datasets = []
@@ -18,24 +27,46 @@ class Project:
         save()
     
     def rename(self, name):
+        """Renames current project.
+        
+        :param name: new name of the project."""
         self.name = name
         #Also change name in path
     
     def add_dataset(self, dataset, path):
+        """Adds a dataset to the dataset list.
+        
+        :param dataset: dataset to add.
+        :param path: path to the dataset.
+        """
+        #Could also do data loading
         self.datasets.append(dataset)
         self.dataset_paths.append(path)
     
     def add_analysis(self, analysis):
+        """Adds an analysis to the analysis list.
+        
+        :param analysis: Analysis to add.
+        """
         self.analysis.append(analysis)
     
     def remove_dataset(self, index):
+        """Removes a dataset from the dataset list.
+        
+        :param index: Index of the dataset to remove.
+        """
         self.datasets.pop(index)
         self.dataset_paths.pop(index)
     
     def remove_analysis(self, index):
+        """Removes an analysis from the analysis list.
+        
+        :param index: Index of the analysis to remove.
+        """
         self.analysis.pop(index)
         
     def save(self):
+        """Saves the current project's data into files."""
         #Save into files, needs implementation
         #Set path to WORKSPACE_PATH/PROJECT_NAME/
         #Save new data to .prj
@@ -54,5 +85,10 @@ class Project:
         for analysis in self.analysis:
             with open('??', 'w+') as file:
                 file.write(analysis)
+    
+    def load_dataset(self,path):
+        """Loads a dataset from the given path.
         
-        
+        :param path: Path of the dataset.
+        """
+        pass
--- src/main/python/project_data/workspace.py
+++ src/main/python/project_data/workspace.py
@@ -1,30 +1,64 @@
-import os
+import os, shutil
 
 class Workspace:
+    """Class representing a workspace, where projects are created and stored."""
     
     def __init__(self, path):
+        """Initializes the workspace given the path.
+        
+        :param path: Absolute path of current workspace.
+        """
         self.path = path
         
     def switch_workspace(self, path):
+        """Switches workspace to indicated path.
+        
+        :param path: New path for the workspace.
+        """
         #Close all projects
         close_project()
         self.path = path
         
     def add_project(self, name):
+        """Adds a project to the current workspace.
+        
+        :param name: Name of the project.
+        """
         new_path = os.path.join(self.path, name)
         if not os.path.exists(new_path):
             os.makedirs(new_path)
+            open_project(name)
         else:
             raise ValueError('There is already a file with this name.')
            
     def open_project(self, name):
+        """Opens the project with the given name.
+        
+        :param name: Name of the project.
+        """
         os.chdir(os.path.join(self.path, name))
         
     def close_project(self):
+        """Closes currently open project."""
         os.chdir(self.path)
         
     def rename_project(self, name, new_name):
+        """Renames a project.
+        
+        :param name: old name of the project.
+        :param new_name: new name for the project.
+        """
         #Closing renaming and opening should work, but slow
         close_project()
         os.rename(name, new_name)
         open_project(new_name)
+    
+    def delete_project(self,name):
+        """Deletes the project with the indicated name.
+        
+        :param name: name of the project to delete.
+        """
+        #DEBUG PRIOR TO TESTING
+        close_project()
+        #delete project folder
+        #shutil.rmtree(self.path + '/' + name)
